9c8c49a636f2652667c4c1538668c2977ee81dd7
evangray7373@gmail.com <evangray7373@gmail.com>
2023-04-16 13:55:22 -0500

    EndScene

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class GameOver : MonoBehaviour
         if (collider.gameObject.tag == "Dog")
         {
             Debug.Log("Game over");
+            SceneManager.LoadScene(2);
         }
     }
 }
